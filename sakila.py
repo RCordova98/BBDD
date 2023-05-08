862e721d8fe7a1c93f7b0fbf31ac086bc6c6fd42
RCordova98 <127288635+RCordova98@users.noreply.github.com>
2023-05-08 20:38:45 +0200

    Update sakila.py

--- sakila.py
+++ sakila.py
@@ -1,7 +1,6 @@
-from sqlalchemy import create_engine, Column, Integer, String, UniqueConstraint, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String, UniqueConstraint, ForeignKey, inspect
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, relationship
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship, declarative_base, joinedload
 
 Base = declarative_base()
 
@@ -24,6 +23,7 @@ class User(Base):
     name = Column(String(50), nullable=False)
     age = Column(Integer)
     email = Column(String(50), nullable=False, unique=True)
+    __table_args__ = (UniqueConstraint('email', name='unique_email'),)
 
 engine = create_engine('mysql+pymysql://root:Basededatos2023@localhost/sakila')
 Base.metadata.create_all(engine)
@@ -33,10 +33,14 @@ session = Session()
 
 def crear_pais():
     nombre_pais = input('Introduce el nombre del país: ')
-    pais = Country(country=nombre_pais)
-    session.add(pais)
+    pais = session.query(Country).filter_by(country=nombre_pais).first()
+    if pais:
+        print(f'Ya existe un país con el nombre {nombre_pais}')
+        return
+    nuevo_pais = Country(country=nombre_pais)
+    session.add(nuevo_pais)
     session.commit()
-    print(f'Se ha creado el país {pais.country}')
+    print(f'Se ha creado el país {nuevo_pais.country}')
 
 def listar_paises():
     paises = session.query(Country).all()
@@ -45,15 +49,35 @@ def listar_paises():
 
 def eliminar_pais():
     nombre_pais = input('Introduce el nombre del país a eliminar: ')
-    pais = session.query(Country).filter_by(country=nombre_pais).one()
-    session.delete(pais)
-    session.commit()
-    print(f'Se ha eliminado el país {pais.country}')
+    pais = session.query(Country).filter_by(country=nombre_pais).one_or_none()
+    if pais is None:
+        print(f'El país {nombre_pais} no se encuentra en la base de datos')
+        return
+    ciudades = session.query(City).filter_by(country_id=pais.country_id).all()
+
+    print(f'Está a punto de eliminar el país {pais.country} y todas las siguientes ciudades relacionadas:')
+    for ciudad in ciudades:
+        print(ciudad.city)
+    confirmacion = input('¿Está seguro que desea eliminar el país y todas sus ciudades relacionadas? (s/n): ')
+    if confirmacion.lower() == 's':
+        for ciudad in ciudades:
+            session.delete(ciudad)
+        session.delete(pais)
+        session.commit()
+        print(f'Se ha eliminado el país {pais.country} y todas sus ciudades relacionadas.')
+    else:
+        print('Operación cancelada')
 
 def crear_ciudad():
     nombre_ciudad = input('Introduce el nombre de la ciudad: ')
     nombre_pais = input('Introduce el nombre del país al que pertenece: ')
     pais = session.query(Country).filter_by(country=nombre_pais).one()
+
+    ciudad_existente = session.query(City).filter_by(city=nombre_ciudad, country_id=pais.country_id).first()
+    if ciudad_existente:
+        print(f'Ya existe una ciudad llamada {nombre_ciudad} en el país {nombre_pais}')
+        return
+
     ciudad = City(city=nombre_ciudad, country=pais)
     session.add(ciudad)
     session.commit()
@@ -66,28 +90,45 @@ def listar_ciudades():
 
 def eliminar_ciudad():
     nombre_ciudad = input('Introduce el nombre de la ciudad a eliminar: ')
-    ciudad = session.query(City).filter_by(city=nombre_ciudad).one()
-    session.delete(ciudad)
-    session.commit()
-    print(f'Se ha eliminado la ciudad {ciudad.city} del país {ciudad.country.country}')
+    nombre_pais = input('Introduce el nombre del país al que pertenece la ciudad: ')
+    ciudad = session.query(City).join(Country).filter(City.city == nombre_ciudad, Country.country == nombre_pais).one_or_none()
+    if ciudad is None:
+        print(f'La ciudad {nombre_ciudad} no se encuentra en la base de datos')
+        return
+    nombre_pais = ciudad.country.country
+    confirmacion = input(f'¿Estás seguro que deseas eliminar la ciudad {ciudad.city} en el país {nombre_pais}? (s/n): ')
+    if confirmacion.lower() == 's':
+        session.delete(ciudad)
+        session.commit()
+        print(f'Se ha eliminado la ciudad {ciudad.city} del país {nombre_pais}')
+    else:
+        print('Operación cancelada')
 
 def crear_tabla_usuarios():
-    class Usuario(Base):
-        __tablename__ = 'usuarios'
-        id = Column(Integer, primary_key=True, autoincrement=True)
-        name = Column(String(50), nullable=False)
-        age = Column(Integer)
-        email = Column(String(50), nullable=False, unique=True)
-        __table_args__ = (UniqueConstraint('email', name='unique_email'),)
-    Base.metadata.create_all(engine)
-    print('Se ha creado la tabla de usuarios')
+    if not inspect(engine).has_table('user'):
+        Base.metadata.create_all(engine)
+        print('Se ha creado la tabla de usuarios')
+    else:
+        print('La tabla de usuarios ya existe')
 
 def borrar_tabla_usuarios():
-    Usuario.table.drop(engine)
-    print('Se ha eliminado la tabla de ususarios')
+    inspector = inspect(engine)
+    if not inspector.has_table('user'):
+        print('La tabla de usuarios no existe')
+    else:
+        Base.metadata.tables["user"].drop(bind=engine, checkfirst=True)
+        print('Se ha eliminado la tabla de usuarios')
 
 def mostrar_estructura_tabla_usuarios():
-    print(Usuario.table)
+    inspector = inspect(engine)
+    if inspector.has_table('user'):
+        insp = inspect(User)
+        columns = insp.columns
+        print("Estructura de la tabla 'User':")
+        for column in columns:
+            print(f"{column.name}: {column.type}")
+    else:
+        print("La tabla no existe en la base de datos")
 
 
 while True:
@@ -104,7 +145,6 @@ while True:
     print('0. Salir')
 
     opcion = input('Seleccione una opción: ')
-
     if opcion == '1':
         crear_pais()
     elif opcion == '2':
